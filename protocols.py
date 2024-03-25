007c3ed9797ed995c825d5f2948632162a245242
Van Horenbeke Noah <noah.vanhorenbeke@student.uclouvain.be>
2024-03-25 11:35:37 +0100

    update script to %

--- protocols.py
+++ protocols.py
@@ -1,35 +1,39 @@
 import pyshark
 import sys
+import pyshark
+
 def count_protocols(pcap_file):
+    total_packets = 0
+    protocols = {}
+
     # Ouvrir la capture de fichiers avec pyshark
     cap = pyshark.FileCapture(pcap_file)
 
-    # Initialiser un dictionnaire pour stocker les comptes des protocoles
-    protocol_counts = {}
-
-    # Parcourir chaque paquet dans la capture
+    # Compter le nombre total de paquets
     for pkt in cap:
-        # Vérifier si le paquet contient une couche de protocole
-        if hasattr(pkt, 'protocol'):
-            protocol = pkt.protocol
-            # Incrémenter le compteur pour ce protocole dans le dictionnaire
-            if protocol in protocol_counts:
-                protocol_counts[protocol] += 1
-            else:
-                protocol_counts[protocol] = 1
+        total_packets += 1
+
+        # Vérifier si le paquet a un attribut 'highest_layer'
+        if hasattr(pkt, 'highest_layer'):
+            protocol = pkt.highest_layer
+            protocols[protocol] = protocols.get(protocol, 0) + 1
 
     # Fermer la capture de fichiers
     cap.close()
 
-    return protocol_counts
+    # Calculer le pourcentage de chaque protocole
+    for protocol, count in protocols.items():
+        percentage = (count / total_packets) * 100
+        protocols[protocol] = percentage
 
-def display_protocol_counts(protocol_counts):
-    print("Protocols and their counts:")
-    for protocol, count in protocol_counts.items():
-        print(f"{protocol}: {count}")
+    return protocols
 
 if __name__ == "__main__":
     file = sys.argv[1]
-    pcap_file = 'Packet/' + file +'.pcapng'   # Remplacer par le chemin de votre fichier de capture
-    protocol_counts = count_protocols(pcap_file)
-    display_protocol_counts(protocol_counts)
+    pcap_file = 'Packet/' + file +'.pcapng' 
+    #pcap_file = 'mon_fichier.pcapng'  # Remplacer par le chemin de votre fichier de capture
+    protocols = count_protocols(pcap_file)
+    for protocol, percentage in protocols.items():
+        print(f"{protocol}: {percentage:.2f}%")
+
+
